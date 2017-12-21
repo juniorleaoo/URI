fc0723d4fb710f1e0f6f49b37f94ce30fab0d7a2
Neilto Ribeiro <juniorleaoo@hotmail.com>
2017-12-21 00:42:03 -0200

    1131, 2717 e SQL 2602, 2603, 2604, 2605

---
+++ 1131.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        int vitoriaGremio = 0, vitoriaInter = 0, empates = 0, golsInter = 0, golsGremio = 0, opcao;
+
+		Scanner scanner = new Scanner(System.in);
+
+		do {
+			golsInter = scanner.nextInt();
+			golsGremio = scanner.nextInt();
+
+			if(golsInter > golsGremio) {
+				vitoriaInter++;
+
+			}else if(golsInter < golsGremio) {
+				vitoriaGremio++;
+
+			}else {
+				empates++;
+			}
+			
+			System.out.println("Novo grenal (1-sim 2-nao)");
+			opcao = scanner.nextInt();
+			
+		}while(opcao == 1);
+		
+		System.out.println(vitoriaInter+vitoriaGremio+empates + " grenais");
+		System.out.println("Inter:"+vitoriaInter);
+		System.out.println("Gremio:"+vitoriaGremio);
+		System.out.println("Empates:"+empates);
+		System.out.println( (vitoriaInter>vitoriaGremio) ? "Inter venceu mais" : (vitoriaInter<vitoriaGremio ? "Gremio venceu mais" : "Nao houve vencedor"));
+ 
+    }
+ 
+}
